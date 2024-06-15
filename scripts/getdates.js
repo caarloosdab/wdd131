062b3ef9014afc62b2c3d00d8a2d9f74f326cdf8
caarloosdab <caarloosdab@gmail.com>
2024-06-14 21:39:26 -0600

    Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+let nLastModif = document.lastModified;
+
+currentyear
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+
+
+const date = document.lastModified
+document.getElementById("lastModified").innerHTML = 'Last modified:  ' + date;
