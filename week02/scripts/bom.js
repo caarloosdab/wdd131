ae6346ab9fa2c860b480e269b017d3f0200ddeb6
caarloosdab <caarloosdab@gmail.com>
2024-06-21 20:06:15 -0600

    DOM events

---
+++ week02/scripts/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+
+button.addEventListener('click', function(){
+    if (input.value.trim() !== '') {  
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        li.textContent = input.value;
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', function (){
+            list.removeChild(li);
+            input.value = '';
+            input.focus()
+        
+        })
+
+    }
+
+})
+
+
+
